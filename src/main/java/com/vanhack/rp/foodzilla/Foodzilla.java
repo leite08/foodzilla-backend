0163ac7645bff8771d0b8f9bc4541898db8c8c5d
Rafael Leite <leite08@gmail.com>
2017-11-28 01:28:57 -0200

    Enabled global CORS access for development

--- src/main/java/com/vanhack/rp/foodzilla/Foodzilla.java
+++ src/main/java/com/vanhack/rp/foodzilla/Foodzilla.java
@@ -2,6 +2,10 @@ package com.vanhack.rp.foodzilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.mashape.unirest.http.Unirest;
 
@@ -11,6 +15,17 @@ public class Foodzilla {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Foodzilla.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	// TODO: define allowed origins and allow access only from them 
+                registry.addMapping("/api/*").allowedOrigins("*");
+            }
+        };
+    }
 	
 	@Override
 	protected void finalize() throws Throwable {
